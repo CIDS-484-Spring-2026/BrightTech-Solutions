3a72c32cab9c27c7aba279e62be7e6d90d4b0655
Lynn Gobina <gobinalynn8364@gmail.com>
2026-03-01 16:54:07 -0600

    Finished styling for the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Footer.css';  
-import logo from '../assets/brightTech_logo.svg';
+import footerLogo from '../assets/footerLogo.svg';
 
 function Footer(){
     return(
         <footer className="footerContainer">
-            <p>hello</p>
+            <p className="footerTitle">BrightTech Solutions</p>
+            <img src={footerLogo} className="footerLogo" />
+            <p><b>Email Us: </b>BrightTechCommunication@gmail.com</p>
+            <p>Our main purpose at BrightTech Solutions is to provide
+            businesses with all of the necesssary tools they need in order to elevate
+            their brand and reach new audiences while maintaining their work ethic 
+            and integrity.
+          </p>
+          <p><b>For immedaite inqueries, call our helpline:</b> (715)-295-1943</p>
+          
         </footer>
     );
 }
